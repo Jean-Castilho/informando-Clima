382510b1f4ea0c34893b876dcc73803799c5d7ea
Jean-Castilho <jeancastilhoSz@proton.me>
2024-01-10 14:56:19 -0300

    localStorage.getItem
    localStorage e um metodo utilizado para
    armazenar informaços é preferencias do usuario

--- APIclima.js
+++ APIclima.js
@@ -1,13 +1,26 @@
 const keyapi = "26fd334d6b582645ee9f618856253924";
 
-
 //funçoes;
+//iniciando espaço de armazenamento no navegador
+
+const citys = JSON.parse(localStorage.getItem("cityAll"));
+
+const cityall = localStorage.getItem("cityAll") !== null ? citys : [];
+
+const geraID = () => {
+  return cityall.length++;
+};
+
+const updatelocal = (cityname) => {
+  localStorage.setItem(geraID(), JSON.stringify(cityname));
+};
+
+//formulario
 
 const validtext = (inputcity) => {
   //validando texto do input .textcity
-  
-  return inputcity.length != 0 ?  true :  false
 
+  return inputcity.length != 0 ? inputcity : false;
 };
 
 const buscandoapi = async (city) => {
@@ -22,12 +35,12 @@ const buscandoapi = async (city) => {
 };
 
 const exibinformations = async (city) => {
+  armazeinformations(city);
+
   const infos = await buscandoapi(city);
 
   console.log(infos);
-  
 };
-
 
 // tags HTML
 
@@ -44,6 +57,8 @@ SearchBtn.addEventListener(
     let namecity = validtext(inputcity.value);
 
     exibinformations(namecity);
+
+    updatelocal(inputcity.value);
 
   }
 );
